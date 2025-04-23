609a38a6fca1b927803ad8cd8533ea4c5e391cac
Lucas Scommegna <scommegnal@hotmail.com>
2025-04-23 10:31:05 -0300

    feat: name uniqueness for heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,6 @@
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
+  validates :name, uniqueness: { scope: :token, case_sensitive: false }
 
   scope :by_token, ->(token) { where(token: token) }
   scope :sorted_by_name, -> { order(name: :asc) }
