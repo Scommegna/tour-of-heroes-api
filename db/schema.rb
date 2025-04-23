6b87bb310572c10e5fe4ce9fce775eeef3eebc00
Lucas Scommegna <scommegnal@hotmail.com>
2025-04-23 09:56:59 -0300

    feat: token field for hero

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_22_175424) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_23_115433) do
   create_table "heroes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token", null: false
+    t.index ["token"], name: "index_heroes_on_token"
   end
 end
